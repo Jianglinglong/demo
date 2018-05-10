d0caedcb995de28c8aa8bc224665507017b2efdc
jiang <2899297045@qq.com>
2018-05-10 21:49:14 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author:Jiang
+ * @date:2018/4/27
+ * @time:15:08
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/info.html")
+    public String info(){
+        return "info";
+    }
+
+    @GetMapping("/own.html")
+    public String own(){
+        return "own";
+    }
+
+    @GetMapping("/quan.html")
+    public String quan(){
+        return "quan";
+    }
+}
